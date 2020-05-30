0caf19f95e6069f9d9941e2ce897f97f0e134b3e
sehouse <sehouse@umich.edu>
2020-05-30 15:14:22 -0400

    Basic frameworking.

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+//NPM INIT
+
+//User will be prompted for Github username and other information
+
 const questions = [
 
 ];
 
 function writeToFile(fileName, data) {
+    //What destination for the file?
+    //Need to create a file with the name fileName
+    //Write to this fileName the data
 }
 
 function init() {
-
+    //Initialize the things that I need here
 }
 
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,16 @@
+//Badge
+//Title
+//Description
+//Table of Contents
+//Installation
+//Usage
+//License
+//Contributing
+//Tests
+//Questions
+//User Github profile picture
+//User Github email
+
 function generateMarkdown(data) {
   return `
 # ${data.title}
